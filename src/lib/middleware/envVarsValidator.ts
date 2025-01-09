54c163dbc21369851f394f033c02a587a8ef1212
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-09 19:41:07 +0900

    amend: Prefer allowlisting over denylisting

--- src/lib/middleware/envVarsValidator.ts
+++ src/lib/middleware/envVarsValidator.ts
@@ -3,27 +3,21 @@ import * as v from "valibot"
 
 import { $EnvVars, type Env } from "@/lib/schema/env"
 
-const redactInputFromIssue = (issue: v.BaseIssue<unknown>) => ({
-    ...issue,
-    input: "<REDACTED>",
-    received: "<REDACTED>",
-})
 export const envVarsValidator = createMiddleware<Env>(async (c, next) => {
     const envVarsParseResult = v.safeParse($EnvVars, c.env)
     if (envVarsParseResult.success) {
         Object.assign(c.env, envVarsParseResult.output)
         await next()
     } else {
-        const { issues } = envVarsParseResult
-        const [headIssue, ...tailIssues] = issues
         console.error({
             // https://developers.cloudflare.com/workers/observability/logs/workers-logs/#best-practices
-            error: new v.ValiError([
-                // issues.map(redactInputFromIssue) と書くと
-                // 型が [T, ...T[]] から T[] にWidening
-                redactInputFromIssue(headIssue),
-                ...tailIssues.map(redactInputFromIssue),
-            ]),
+            error: {
+                name: "ValiError",
+                issues: envVarsParseResult.issues.map((issue) => ({
+                    message: issue.message,
+                    dotPath: v.getDotPath(issue),
+                })),
+            },
         })
         return c.text("Internal Server Error", 500)
     }
