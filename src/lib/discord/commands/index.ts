5e3e0951aa8b975bdd9272b04c9356d71dcf0946
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-11 23:17:50 +0900

    amend: Remove postRules from imports

--- src/lib/discord/commands/index.ts
+++ src/lib/discord/commands/index.ts
@@ -5,10 +5,6 @@ export * as config from "./configCommand"
 /**
  * @package
  */
-export * as postRules from "./postRulesCommand"
-/**
- * @package
- */
 export * as help from "./helpCommand"
 /**
  * @package
