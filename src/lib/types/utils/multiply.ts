2332bb5ee4242471a18ebf2a07bef2420d174e54
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-21 22:25:40 +0900

    Remove no longer needed ts-expect-error comment in multiply.ts

--- src/lib/types/utils/multiply.ts
+++ src/lib/types/utils/multiply.ts
@@ -4,11 +4,5 @@ import type { RepeatArray } from "./repeatArray"
 
 // 非負整数どうしのみ
 export type Multiply<M extends number, N extends number> = M extends infer _M extends number
-    ? // @ts-expect-error 定義では出るが、それぞれの参照では出ないエラー
-      // 型 '"length"' と 'keyof RepeatArray<BuildTuple<_M>, N>' を比較するスタックが深すぎます。ts(2321)
-      //    https://susisu.hatenablog.com/entry/2020/09/12/214343 オブジェクトのプロパティ内で再帰する が使えるかも
-      // 型 '"length"' はインデックスの種類 'RepeatArray<BuildTuple<_M>, N>' に使用できません。ts(2536)
-      //    (たぶん) コンパイラの敗北
-      // TODO: とりあえず再帰制限をなんとかする
-      RepeatArray<BuildTuple<_M>, N>["length"]
+    ? RepeatArray<BuildTuple<_M>, N>["length"]
     : never
