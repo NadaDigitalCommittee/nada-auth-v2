bd8df322d4ff3c993cbe05609396eeae4867ad27
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-28 17:59:25 +0900

    `Multiply`: Fix union distribution process

--- src/lib/types/utils/multiply.ts
+++ src/lib/types/utils/multiply.ts
@@ -3,6 +3,6 @@ import type { BuildTuple } from "type-fest/source/internal"
 import type { RepeatArray } from "./repeatArray"
 
 // 非負整数どうしのみ
-export type Multiply<M extends number, N extends number> = M extends infer _M extends number
-    ? RepeatArray<BuildTuple<_M>, N>["length"]
+export type Multiply<M extends number, N extends number> = M extends M
+    ? RepeatArray<BuildTuple<M>, N>["length"]
     : never
