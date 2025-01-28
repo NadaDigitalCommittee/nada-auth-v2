b2481e8768dc0dab1ac240d776d6c763062dad4b
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-28 18:31:32 +0900

    `Reverse`: Add tests

---
+++ src/lib/types/utils/tests/reverse.test.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { test } from "bun:test"
+import { expectTypeOf } from "expect-type"
+
+import type { Reverse } from "../reverse"
+
+test("Reverse:base", () => {
+    expectTypeOf(null! as Reverse<[42, "string", boolean, object, 334n]>).toMatchTypeOf<
+        [334n, object, boolean, "string", 42]
+    >()
+})
