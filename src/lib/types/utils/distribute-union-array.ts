eb5d60bd4226bcbbd31d7cb06ae2560acc6f22d2
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-01-28 17:55:45 +0900

    Refactor utility types `FromEntries` and `DistributeUnionArray` and add tests

--- src/lib/types/utils/distribute-union-array.ts
+++ src/lib/types/utils/distribute-union-array.ts
@@ -1,11 +1,14 @@
-import type { IsTuple } from "type-fest"
+import type { IsNever, IsTuple } from "type-fest"
 
 // https://github.com/type-challenges/type-challenges/issues/11761
 export type DistributeUnionArray<T extends unknown[]> =
     IsTuple<T> extends true
         ? T extends [infer Head, ...infer Tail]
-            ? Head extends Head
+            ? // HeadがneverのときにHead extends Headの部分で空のユニオンを分配することになってneverが返ってしまうのを防ぐ
+              IsNever<Head> extends true
                 ? [Head, ...DistributeUnionArray<Tail>]
-                : never
+                : Head extends Head
+                  ? [Head, ...DistributeUnionArray<Tail>]
+                  : never
             : []
         : T
