b2cb65e5731566ca6082411aab311eeb07e781da
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-26 00:15:42 +0900

    feat(app/authn): Use `app.use()` for `reactRenderer` and insert `<App>`

--- src/app/authn/index.tsx
+++ src/app/authn/index.tsx
@@ -15,6 +15,8 @@ import type { ReactNode } from "react"
 import * as v from "valibot"
 
 import type { AppType } from "@/app"
+import { App } from "@/components/App"
+import { appSteps } from "@/components/AppStepper"
 import { createLayout } from "@/components/layout"
 import { type ErrorContext, reportErrorWithContext } from "@/lib/discord/utils"
 import type { Env } from "@/lib/schema/env"
@@ -26,7 +28,7 @@ const ErrorAlert = ({ title, description }: { title: ReactNode; description: Rea
     <Alert
         severity="error"
         css={css`
-            max-width: 70rem;
+            width: 100%;
         `}
     >
         <AlertTitle>{title}</AlertTitle>
@@ -34,109 +36,107 @@ const ErrorAlert = ({ title, description }: { title: ReactNode; description: Rea
     </Alert>
 )
 
-const app = new Hono<Env>().get(
-    "/",
-    reactRenderer(({ children }) =>
-        createLayout({ head: <title>Google でサインイン</title> })({
-            children: (
-                <main
-                    css={css`
-                        padding: 1rem;
-                    `}
-                >
-                    {children}
-                </main>
-            ),
-        }),
-    ),
-    vValidator(
-        "query",
-        v.object({
-            token: $RequestToken,
-        }),
-        (result, c) => {
-            if (!result.success) {
+const STEP = 0
+
+const app = new Hono<Env>()
+    .use(
+        reactRenderer(({ children }) =>
+            createLayout({ head: <title>{appSteps[STEP].nameVerbose}</title> })({
+                children: <App activeStep={STEP}>{children}</App>,
+            }),
+        ),
+    )
+    .get(
+        "/",
+        vValidator(
+            "query",
+            v.object({
+                token: $RequestToken,
+            }),
+            (result, c) => {
+                if (!result.success) {
+                    c.status(400)
+                    return c.render(
+                        <ErrorAlert
+                            title="Bad Request"
+                            description={<span>トークンが指定されませんでした。</span>}
+                        ></ErrorAlert>,
+                    )
+                }
+            },
+        ),
+        async (c) => {
+            const authNRequestRecord = c.env.AuthNRequests
+            const sessionRecord = c.env.Sessions
+            const { rest } = c.var
+            const sessionId = await (async () => {
+                const requestToken = c.req.valid("query").token
+                const kvSessionIdKey =
+                    `requestToken:${requestToken}` satisfies keyof AuthNRequestRecord
+                const kvSessionId = await authNRequestRecord.get(kvSessionIdKey)
+                if (kvSessionId) {
+                    await authNRequestRecord.delete(kvSessionIdKey)
+                    setCookie(c, sharedCookieNames.sessionId, kvSessionId, {
+                        ...sharedCookieOption,
+                        sameSite: "Lax",
+                    })
+                    return kvSessionId
+                } else return getCookie(c, sharedCookieNames.sessionId)
+            })()
+            if (!sessionId) {
                 c.status(400)
                 return c.render(
                     <ErrorAlert
                         title="Bad Request"
-                        description={<span>トークンが指定されませんでした。</span>}
+                        description={<span>トークンが無効です。</span>}
                     ></ErrorAlert>,
                 )
             }
-        },
-    ),
-    async (c) => {
-        const authNRequestRecord = c.env.AuthNRequests
-        const sessionRecord = c.env.Sessions
-        const { rest } = c.var
-        const sessionId = await (async () => {
-            const requestToken = c.req.valid("query").token
-            const kvSessionIdKey = `requestToken:${requestToken}` satisfies keyof AuthNRequestRecord
-            const kvSessionId = await authNRequestRecord.get(kvSessionIdKey)
-            if (kvSessionId) {
-                await authNRequestRecord.delete(kvSessionIdKey)
-                setCookie(c, sharedCookieNames.sessionId, kvSessionId, {
-                    ...sharedCookieOption,
-                    sameSite: "Lax",
-                })
-                return kvSessionId
-            } else return getCookie(c, sharedCookieNames.sessionId)
-        })()
-        if (!sessionId) {
-            c.status(400)
-            return c.render(
-                <ErrorAlert
-                    title="Bad Request"
-                    description={<span>トークンが無効です。</span>}
-                ></ErrorAlert>,
+            const rawSession = await sessionRecord.get(sessionId, "json").catch(shouldBeError)
+            const sessionParseResult = v.safeParse($Session, rawSession)
+            if (!sessionParseResult.success) {
+                c.status(400)
+                return c.render(
+                    <ErrorAlert
+                        title="Bad Request"
+                        description={<span>セッションが無効です。</span>}
+                    ></ErrorAlert>,
+                )
+            }
+            const session = sessionParseResult.output
+            const errorContext = {
+                guildId: session.guildId,
+                user: session.user,
+            } as const satisfies ErrorContext
+            const originalInteractionResRoute = Routes.webhookMessage(
+                c.env.DISCORD_APPLICATION_ID,
+                session.interactionToken,
+                "@original",
             )
-        }
-        const rawSession = await sessionRecord.get(sessionId, "json").catch(shouldBeError)
-        const sessionParseResult = v.safeParse($Session, rawSession)
-        if (!sessionParseResult.success) {
-            c.status(400)
-            return c.render(
-                <ErrorAlert
-                    title="Bad Request"
-                    description={<span>セッションが無効です。</span>}
-                ></ErrorAlert>,
-            )
-        }
-        const session = sessionParseResult.output
-        const errorContext = {
-            guildId: session.guildId,
-            user: session.user,
-        } as const satisfies ErrorContext
-        const originalInteractionResRoute = Routes.webhookMessage(
-            c.env.DISCORD_APPLICATION_ID,
-            session.interactionToken,
-            "@original",
-        )
-        const originalResponse = (await rest
-            .get(originalInteractionResRoute)
-            .catch(async (e: unknown) => {
-                if (e instanceof Error) await reportErrorWithContext(e, errorContext, c.env)
-                return null
-            })) as RESTPatchAPIWebhookWithTokenMessageResult | null
-        if (originalResponse?.components?.length) {
-            await rest
-                .patch(originalInteractionResRoute, {
-                    body: {
-                        content: ":tickets: リンクが使用されました。",
-                        components: [],
-                    } satisfies RESTPatchAPIWebhookWithTokenMessageJSONBody,
-                })
+            const originalResponse = (await rest
+                .get(originalInteractionResRoute)
                 .catch(async (e: unknown) => {
                     if (e instanceof Error) await reportErrorWithContext(e, errorContext, c.env)
-                })
-        }
-        const honoClient = hc<AppType>(new URL(c.req.url).origin)
-        const apiOAuthUrl: URL = honoClient.api.oauth.$url()
-        apiOAuthUrl.protocol = "https:"
-        return c.redirect(apiOAuthUrl)
-    },
-)
+                    return null
+                })) as RESTPatchAPIWebhookWithTokenMessageResult | null
+            if (originalResponse?.components?.length) {
+                await rest
+                    .patch(originalInteractionResRoute, {
+                        body: {
+                            content: ":tickets: リンクが使用されました。",
+                            components: [],
+                        } satisfies RESTPatchAPIWebhookWithTokenMessageJSONBody,
+                    })
+                    .catch(async (e: unknown) => {
+                        if (e instanceof Error) await reportErrorWithContext(e, errorContext, c.env)
+                    })
+            }
+            const honoClient = hc<AppType>(new URL(c.req.url).origin)
+            const apiOAuthUrl: URL = honoClient.api.oauth.$url()
+            apiOAuthUrl.protocol = "https:"
+            return c.redirect(apiOAuthUrl)
+        },
+    )
 
 /**
  * @package
