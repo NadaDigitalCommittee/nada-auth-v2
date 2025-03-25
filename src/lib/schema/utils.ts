b8cafab9bf0a28d53ac09cb7bd2a018293272f03
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-03-26 01:16:41 +0900

    feat(schema/utils): Add utility `generateSchema`

--- src/lib/schema/utils.ts
+++ src/lib/schema/utils.ts
@@ -1,5 +1,7 @@
 import * as v from "valibot"
 
+import { isPlainObject } from "../utils/object"
+
 export type UnknownValibotSchema = v.BaseSchema<unknown, unknown, v.BaseIssue<unknown>>
 
 export const vFormFieldValidator = (schema: UnknownValibotSchema) => (value: unknown) => {
@@ -9,3 +11,35 @@ export const vFormFieldValidator = (schema: UnknownValibotSchema) => (value: unk
     })
     return parseResult.success || parseResult.issues[0].message
 }
+
+export const generateSchema = <const T>(obj: T) =>
+    v.lazy((() => {
+        switch (typeof obj) {
+            case "bigint":
+            case "boolean":
+            case "number":
+            case "string":
+            case "symbol":
+                return Number.isNaN(obj) ? v.nan() : v.literal(obj)
+            case "undefined":
+                return v.undefined()
+            case "object":
+                if (obj === null) {
+                    return v.null()
+                } else if (Array.isArray(obj)) {
+                    return v.tuple(obj.map(generateSchema))
+                } else if (isPlainObject(obj)) {
+                    return v.object(
+                        Object.fromEntries(
+                            Object.entries(obj).map(([key, value]) => [key, generateSchema(value)]),
+                        ),
+                    )
+                } else {
+                    return v.unknown()
+                }
+            case "function":
+                return v.function()
+            default:
+                return v.unknown()
+        }
+    }) as () => v.GenericSchema<T>)
