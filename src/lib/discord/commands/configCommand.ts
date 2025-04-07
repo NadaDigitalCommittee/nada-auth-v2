8432039128639a67ada6142e6b9945b326fa66cf
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-04-07 23:47:46 +0900

    feat: Create slash command group: `/config sheets`

--- src/lib/discord/commands/configCommand.ts
+++ src/lib/discord/commands/configCommand.ts
@@ -8,10 +8,13 @@ import {
     type APIApplicationCommandInteractionDataBooleanOption,
     type APIApplicationCommandInteractionDataSubcommandGroupOption,
     type APIApplicationCommandSubcommandOption,
+    type APIButtonComponentWithURL,
     type APIEmbedField,
     ApplicationCommandOptionType,
     ApplicationCommandType,
+    ButtonStyle,
     ChannelType,
+    ComponentType,
     InteractionContextType,
     PermissionFlagsBits,
     type RESTPatchAPIWebhookJSONBody,
@@ -21,7 +24,9 @@ import {
     type RESTPostAPIChatInputApplicationCommandsJSONBody,
     Routes,
 } from "discord-api-types/v10"
-import { type CommandHandler, Embed } from "discord-hono"
+import { type CommandHandler, Components, Embed } from "discord-hono"
+import { google } from "googleapis"
+import { hc } from "hono/client"
 import type { ArrayValues } from "type-fest"
 import * as v from "valibot"
 
@@ -29,6 +34,9 @@ import {
     guildConfigInit,
     guildConfigKvKeyToOptionNameMap,
     guildConfigOptionNameToKvKeyMap,
+    requestTokenExpirationTtl,
+    sessionExpirationTtl,
+    sessionExpirationTtlDev,
 } from "../constants"
 import {
     type CommandInteractionDataBasicOptionTypeToOptionValueType,
@@ -37,10 +45,12 @@ import {
     reportErrorWithContext,
 } from "../utils"
 
+import type { AppType } from "@/app"
 import type { Env } from "@/lib/schema/env"
-import { $GuildConfig, type GuildConfig } from "@/lib/schema/kvNamespaces"
+import { $GuildConfig, type GuildConfig, type SheetsOAuthSession } from "@/lib/schema/kvNamespaces"
 import type { MapKeyOf } from "@/lib/types/utils/map"
 import { id } from "@/lib/utils/fp"
+import { generateSecret } from "@/lib/utils/secret"
 
 const configSetOptions = [
     {
@@ -136,6 +146,39 @@ export const command = {
                     name: "force",
                     description: "エラーを無視して初期化（既定値: False）",
                     type: ApplicationCommandOptionType.Boolean,
+                },
+            ],
+        },
+        {
+            name: "sheets",
+            description:
+                "スプレッドシートを連携すると、ロールやニックネームを自動で割り当てることができます。",
+            type: ApplicationCommandOptionType.SubcommandGroup,
+            options: [
+                {
+                    name: "init",
+                    description: "スプレッドシートを新規に作成し、アプリにアクセス権限を与えます。",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [],
+                },
+                {
+                    name: "show",
+                    description: "連携しているスプレッドシートを表示します。",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [],
+                },
+                {
+                    name: "revoke",
+                    description: "スプレッドシートとの連携を解除します。",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    options: [
+                        {
+                            type: ApplicationCommandOptionType.Boolean,
+                            name: "hard",
+                            description: "ファイルも破棄する（既定値: False）",
+                            required: false,
+                        },
+                    ],
                 },
             ],
         },
@@ -377,7 +420,99 @@ export const handler: CommandHandler<Env> = async (c) => {
                 embeds: [generateConfigTableEmbed(guildConfigInit)],
             })
         }
-
+        case "sheets init": {
+            if (guildConfig._sheet?.spreadsheetId) {
+                return c.res(
+                    `:warning: このサーバーには、すでに[連携されているスプレッドシート](https://docs.google.com/spreadsheets/d/${guildConfig._sheet.spreadsheetId})があります。上書きしようとしている場合は、先にこれを破棄してください。`,
+                )
+            }
+            const interactionToken = c.interaction.token
+            const requestToken = generateSecret(64)
+            const sessionId = generateSecret(64)
+            const session: SheetsOAuthSession = { guildId, interactionToken }
+            await c.env.AuthNRequests.put(`requestToken:${requestToken}`, sessionId, {
+                expirationTtl: requestTokenExpirationTtl,
+            })
+            await c.env.Sessions.put(sessionId, JSON.stringify(session), {
+                expirationTtl: import.meta.env.DEV ? sessionExpirationTtlDev : sessionExpirationTtl,
+            })
+            const honoClient = hc<AppType>(c.env.ORIGIN)
+            const oAuthUrl = honoClient.oauth.sheets.$url({ query: { token: requestToken } })
+            const oAuthButtonLink = {
+                label: "アクセスを許可",
+                type: ComponentType.Button,
+                style: ButtonStyle.Link,
+                emoji: c.env.DISCORD_APPLICATION_EMOJIS.g_logo,
+                url: oAuthUrl.href,
+            } as const satisfies APIButtonComponentWithURL
+            return c.ephemeral(true).res({
+                content: `:person_tipping_hand: アクセス許可が必要です。下のボタンからアプリにアクセス権を与えてください。
+発行されたリンクは ${requestTokenExpirationTtl} 秒間、1 度だけ有効です。`,
+                components: new Components().row(oAuthButtonLink),
+            })
+        }
+        case "sheets show": {
+            if (!guildConfig._sheet?.spreadsheetId) {
+                return c.res(":warning: 連携されているスプレッドシートがありません。")
+            }
+            const sheetButtonLink = {
+                label: "スプレッドシートを開く",
+                type: ComponentType.Button,
+                style: ButtonStyle.Link,
+                url: `https://docs.google.com/spreadsheets/d/${guildConfig._sheet.spreadsheetId}`,
+            } as const satisfies APIButtonComponentWithURL
+            return c.res({
+                components: new Components().row(sheetButtonLink),
+            })
+        }
+        case "sheets revoke": {
+            if (!guildConfig._sheet?.spreadsheetId) {
+                return c.res(":warning: 連携されているスプレッドシートがありません。")
+            }
+            const [{ options: sheetsRevokeOptions }] = (
+                options[0] as APIApplicationCommandInteractionDataSubcommandGroupOption
+            ).options as [
+                {
+                    name: string
+                    type: ApplicationCommandOptionType.Subcommand
+                    options: [APIApplicationCommandInteractionDataBooleanOption] | []
+                },
+            ]
+            const hard = sheetsRevokeOptions[0]?.value ?? false
+            if (hard) {
+                const oAuth2Client = new google.auth.OAuth2({
+                    clientId: c.env.GOOGLE_OAUTH_CLIENT_ID,
+                    clientSecret: c.env.GOOGLE_OAUTH_CLIENT_SECRET,
+                    credentials: {
+                        access_token: guildConfig._sheet.accessToken,
+                        refresh_token: guildConfig._sheet.refreshToken,
+                    },
+                })
+                if (guildConfig._sheet.accessTokenExpiry <= Date.now()) {
+                    const { credentials } = await oAuth2Client.refreshAccessToken()
+                    /* eslint-disable @typescript-eslint/no-non-null-assertion */
+                    guildConfig._sheet.accessToken = credentials.access_token!
+                    guildConfig._sheet.accessTokenExpiry = credentials.expiry_date!
+                    /* eslint-enable @typescript-eslint/no-non-null-assertion */
+                }
+                const drive = google.drive({ version: "v2", auth: oAuth2Client })
+                const spreadsheetDeleteResponse = await drive.files
+                    .trash({
+                        fileId: guildConfig._sheet.spreadsheetId,
+                        supportsAllDrives: true,
+                    })
+                    .catch(id<unknown, Error>)
+                if (spreadsheetDeleteResponse instanceof Error) {
+                    await reportErrorWithContext(spreadsheetDeleteResponse, errorContext, c.env)
+                    return c.res(":x: ファイルをごみ箱に移動することができませんでした。")
+                }
+            }
+            delete guildConfig._sheet.spreadsheetId
+            await guildConfigRecord.put(guildId, JSON.stringify(guildConfig))
+            return c.res(
+                `:white_check_mark: ${hard ? "ファイルがごみ箱に移動され、" : ""}スプレッドシートとの連携が解除されました。`,
+            )
+        }
         default:
             return c.res(":x: このサブコマンドはサポートされていません。")
     }
