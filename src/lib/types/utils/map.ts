3f9abc3e282b5177a5ab3cdc4d06e7dd3288fc27
ghaaj <91141283+ghaaj@users.noreply.github.com>
2025-04-17 17:09:17 +0900

    refactor: Use `UnknownMap` from type-fest

--- src/lib/types/utils/map.ts
+++ src/lib/types/utils/map.ts
@@ -1,5 +1,4 @@
-import type { Writable } from "type-fest"
+import type { UnknownMap } from "type-fest"
 
-type UnknownMap = Map<unknown, unknown> | ReadonlyMap<unknown, unknown>
-export type MapKeyOf<M extends UnknownMap> = Writable<M> extends Map<infer K, unknown> ? K : never
-export type MapValueOf<M extends UnknownMap> = Writable<M> extends Map<unknown, infer V> ? V : never
+export type MapKeyOf<M extends UnknownMap> = M extends ReadonlyMap<infer K, unknown> ? K : never
+export type MapValueOf<M extends UnknownMap> = M extends ReadonlyMap<unknown, infer V> ? V : never
